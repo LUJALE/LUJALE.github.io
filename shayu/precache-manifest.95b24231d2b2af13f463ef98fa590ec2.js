d94e0c3b32b8dfa7f3dc008039e7a0e679d9efd1
le <969307733@qq.com>
2019-07-29 16:10:48 +0800

    s

---
+++ shayu/precache-manifest.95b24231d2b2af13f463ef98fa590ec2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733c3d00456888a45d53dcf72cd6aedf",
+    "url": "/shayu/index.html"
+  },
+  {
+    "revision": "9a26f51ac9a1274fafc5",
+    "url": "/shayu/static/css/main.f8676822.chunk.css"
+  },
+  {
+    "revision": "6eb58e62c745969d09a9",
+    "url": "/shayu/static/js/2.51ddb188.chunk.js"
+  },
+  {
+    "revision": "9a26f51ac9a1274fafc5",
+    "url": "/shayu/static/js/main.c17ce6b9.chunk.js"
+  },
+  {
+    "revision": "b8097fc458caf190355b",
+    "url": "/shayu/static/js/runtime~main.85d0232d.js"
+  },
+  {
+    "revision": "f763e31b1dd1277d735a566351ed0dc8",
+    "url": "/shayu/static/media/add.f763e31b.png"
+  },
+  {
+    "revision": "bfac3bdb91550ff01864022d14ad0178",
+    "url": "/shayu/static/media/shayu.bfac3bdb.jpg"
+  },
+  {
+    "revision": "2b64fcc42a478425ed43744367e3073b",
+    "url": "/shayu/static/media/welcome.2b64fcc4.png"
+  }
+]);
